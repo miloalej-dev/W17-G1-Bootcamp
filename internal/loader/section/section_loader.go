1fe335bb97bb6215089f2f45db9738bbcba696a3
bocastellano_meli <boris.castellanos@mercadolibre.com.co>
2025-07-01 08:36:10 -0500

    feat: section loader json

--- internal/loader/section/section_loader.go
+++ internal/loader/section/section_loader.go
@@ -1,1 +1,8 @@
 package loader
+
+import "github.com/miloalej-dev/W17-G1-Bootcamp/pkg/models"
+
+// Loader is an interface that represents the loader
+type SectionLoader interface {
+	Load() (v map[int]models.Section, err error)
+}
