8b884bdb0a9e2621a4b891c70413a3da4f6dcd30
jalvarezbelt_meli <juan.alvarezbeltran@mercadolibre.com.co>
2025-07-02 10:08:20 -0500

    Modify(products): Added required fields for model product : Validation

--- pkg/models/product.go
+++ pkg/models/product.go
@@ -4,25 +4,25 @@ type Product struct {
 	//Identificador único de producto
 	ID int `json:"id"`
 	// Un código de producto
-	ProductCode string `json:"product_code"`
+	ProductCode string `json:"product_code,validate:required"`
 	// Una descripción
-	Description string `json:"description"`
+	Description string `json:"description,validate:required"`
 	// Una descripción
-	Width float64 `json:"width"`
+	Width float64 `json:"width,validate:required"`
 	// Una dimensión de alto
-	Height float64 `json:"height"`
+	Height float64 `json:"height,validate:required"`
 	//Una dimensión de largo
-	Length float64 `json:"lenght"`
+	Length float64 `json:"lenght,validate:required"`
 	//Un peso neto
-	NetWeight float64 `json:"net_weight"`
+	NetWeight float64 `json:"net_weight,validate:required"`
 	// Un ratio de vencimiento
-	ExpirationRate float64 `json:"expiration_rate"`
+	ExpirationRate float64 `json:"expiration_rate,validate:required"`
 	// Una temperatura de congelación recomendada
-	RecommendedFreezingTemperature float64 `json:"recommended_freezing_temperature"`
+	RecommendedFreezingTemperature float64 `json:"recommended_freezing_temperature,validate:required"`
 	// Un ratio de congelación, el cual indica que tan rápido se congelara
-	FreezingRate float64 `json:"freezing_rate"`
+	FreezingRate float64 `json:"freezing_rate,validate:required"`
 	// Un Tipo de producto(ProductType) asociado
-	ProductTypeID int `json:"product_type_id"`
+	ProductTypeID int `json:"product_type_id,validate:required"`
 	// Un Proveedor (Seller) asociado (NO es OBLIGATORIA)
 	SellerID int `json:"seller_id,omitempty"`
 }
