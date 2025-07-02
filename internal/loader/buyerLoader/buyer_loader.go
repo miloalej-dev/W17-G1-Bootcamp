9b930cc3142d6babd681ff5657973d1d62182930
Armando Serna <armando.serna@mercadolibre.com.co>
2025-07-02 15:09:13 -0500

    refator: eliminate buyer loader

--- internal/loader/buyerLoader/buyer_loader.go
+++
@@ -1,8 +0,0 @@
-package buyerLoader
-
-import "github.com/miloalej-dev/W17-G1-Bootcamp/pkg/models"
-
-// Loader is an interface that represents the loader
-type Loader interface {
-	Load() (v map[int]models.Buyer, err error)
-}
