b654f8a641e9798cbb7fc467b10a92d8a13483f0
Camilo Nossa <camilo.nossa@mercadolibre.com.co>
2025-07-02 13:32:26 -0500

    chore(loader): remove duplicated interfaces

--- internal/loader/product/product_loader.go
+++
@@ -1,7 +0,0 @@
-package loaderProduct
-
-import "github.com/miloalej-dev/W17-G1-Bootcamp/pkg/models"
-
-type Loader interface {
-	Load() (v map[int]models.Product, err error)
-}
--- internal/loader/warehouse/warehouse_loader.go
+++
@@ -1,7 +0,0 @@
-package loaderWarehouse
-
-import "github.com/miloalej-dev/W17-G1-Bootcamp/pkg/models"
-
-type Loader interface {
-	Load() (warehouses map[int]models.Warehouse, err error)
-}
