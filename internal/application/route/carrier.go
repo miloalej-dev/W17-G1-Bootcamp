09ed710043051fff9111187064bdfc6aa30a766e
jparraareval_meli <juan.parraarevalo@mercadolibre.com.co>
2025-07-15 17:13:59 -0500

    refactor: moved locality logic away from carrier

--- internal/application/route/carrier.go
+++ internal/application/route/carrier.go
@@ -9,7 +9,4 @@ func CarrierRoutes(router chi.Router, handler *handler.CarrierDefault) {
 	router.Route("/api/v1/carriers", func(rt chi.Router) {
 		rt.Post("/", handler.PostCarrier)
 	})
-	router.Route("/api/v1/localities", func(rt chi.Router) {
-		rt.Get("/reportCarriers", handler.GetCarrier)
-	})
 }
