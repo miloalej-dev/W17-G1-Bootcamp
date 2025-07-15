a63b7e69908833d0f4439c997ecbc172f13f91a9
Camilo Alejandro <camilo.nossa@mercadolibre.com.co>
2025-07-15 11:23:12 -0500

    refactor: remove redundant comment frescos_ddl.sql

--- docs/db/scripts/frescos_ddl.sql
+++ docs/db/scripts/frescos_ddl.sql
@@ -6,9 +6,6 @@ SET @OLD_SQL_MODE = @@SQL_MODE, SQL_MODE =
         'ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
 
 -- -----------------------------------------------------
--- Schema mydb
--- -----------------------------------------------------
--- -----------------------------------------------------
 -- Schema frescos
 -- -----------------------------------------------------
 DROP SCHEMA IF EXISTS `frescos`;
