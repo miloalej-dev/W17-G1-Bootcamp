d5c3b30435966be27ac8bcb92b9e931e5cd698a1
Camilo Nossa <camilo.nossa@mercadolibre.com.co>
2025-07-15 23:18:16 -0500

    feat: add methods to retrieve inbound orders report in EmployeeService

--- internal/service/employee.go
+++ internal/service/employee.go
@@ -1,4 +1,20 @@
 package service
 
+import "github.com/miloalej-dev/W17-G1-Bootcamp/pkg/models"
+
 type EmployeeService interface {
+	RetrieveAll() (e []models.Employee, err error)
+
+	Retrieve(id int) (models.Employee, error)
+
+	Register(emp models.Employee) (models.Employee, error)
+	Modify(emp models.Employee) (models.Employee, error)
+
+	PartialModify(id int, fields map[string]interface{}) (models.Employee, error)
+
+	Remove(id int) error
+
+	RetrieveInboundOrdersReport() ([]models.EmployeeInboundOrdersReport, error)
+
+	RetrieveInboundOrdersReportById(id int) (models.EmployeeInboundOrdersReport, error)
 }
