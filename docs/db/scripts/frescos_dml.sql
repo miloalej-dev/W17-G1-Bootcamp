007eb9472ca05335a388bde417346dea482caa65
jparraareval_meli <juan.parraarevalo@mercadolibre.com.co>
2025-07-15 19:02:40 -0500

    feature: add carriers data

--- docs/db/scripts/frescos_dml.sql
+++ docs/db/scripts/frescos_dml.sql
@@ -15,3 +15,26 @@ insert into buyers (id, card_number_id, first_name, last_name) values (7, '730-4
 insert into buyers (id, card_number_id, first_name, last_name) values (8, '162-07-6620', 'Lucius', 'Durdle');
 insert into buyers (id, card_number_id, first_name, last_name) values (9, '750-60-2271', 'Abbye', 'Wedmore');
 insert into buyers (id, card_number_id, first_name, last_name) values (10, '620-25-5585', 'Genny', 'Mothersole');
+
+INSERT INTO carriers (id, cid, name, address, telephone, locality_id) VALUES
+(1, 'CID#1', 'Thoughtstorm', 'PO Box 22954', '288-738-3936', 1),
+(2, 'CID#2', 'Browsezoom', 'Suite 35', '940-137-0407', 1),
+(3, 'CID#3', 'Skipstorm', 'Suite 74', '840-873-2923', 1),
+(4, 'CID#4', 'Avamm', 'Apt 640', '847-917-8225', 1),
+(5, 'CID#5', 'Eire', 'PO Box 56757', '384-527-6343', 1),
+(6, 'CID#6', 'JumpXS', '2nd Floor', '551-728-3925', 1),
+(7, 'CID#7', 'Mycat', 'Room 1041', '341-238-3077', 1),
+(8, 'CID#8', 'Tagopia', 'Apt 277', '568-723-2205', 1),
+(9, 'CID#9', 'Edgetag', '19th Floor', '811-992-3510', 1),
+(10, 'CID#10', 'Edgeify', 'Apt 450', '680-505-0391', 1),
+(11, 'CID#11', 'Meevee', '16th Floor', '181-434-3837', 1),
+(12, 'CID#12', 'Mydeo', 'PO Box 4575', '193-669-2263', 1),
+(13, 'CID#13', 'Dynava', 'Apt 197', '538-860-0102', 1),
+(14, 'CID#14', 'Miboo', '2nd Floor', '915-701-5966', 1),
+(15, 'CID#15', 'Yoveo', 'Apt 776', '958-369-9070', 1),
+(16, 'CID#16', 'Tambee', 'Apt 107', '359-918-1051', 1),
+(17, 'CID#17', 'Skidoo', 'Apt 1600', '838-491-7980', 1),
+(18, 'CID#18', 'Yabox', 'PO Box 96159', '922-135-9305', 1),
+(19, 'CID#19', 'Meedoo', 'PO Box 56809', '971-217-9192', 1),
+(20, 'CID#20', 'Jaxnation', 'Apt 802', '747-275-6029', 1);
+
