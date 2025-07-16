173f818557566f766b1cc03cefdc4d46512b1db7
Camilo Nossa <camilo.nossa@mercadolibre.com.co>
2025-07-15 23:18:09 -0500

    feat: add inbound orders report methods to EmployeeRepository

--- internal/repository/employee.go
+++ internal/repository/employee.go
@@ -4,4 +4,6 @@ import "github.com/miloalej-dev/W17-G1-Bootcamp/pkg/models"
 
 type EmployeeRepository interface {
 	Repository[int, models.Employee]
+	InboundOrdersReport() ([]models.EmployeeInboundOrdersReport, error)
+	InboundOrdersReportById(id int) (models.EmployeeInboundOrdersReport, error)
 }
