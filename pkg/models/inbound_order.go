6c886c4b53df96fda7f464896be6582606a8f435
Camilo Nossa <camilo.nossa@mercadolibre.com.co>
2025-07-20 22:08:03 -0500

    feat: add OrderDate field to InboundOrder model and update constructor

--- pkg/models/inbound_order.go
+++ pkg/models/inbound_order.go
@@ -1,17 +1,21 @@
 package models
 
+import "time"
+
 type InboundOrder struct {
-	Id             int    `json:"id" gorm:"primaryKey"`
-	OrderNumber    string `json:"order_number"`
-	EmployeeId     int    `json:"employee_id"`
-	ProductBatchId int    `json:"product_batch_id"`
-	WarehouseId    int    `json:"warehouse_id"`
+	Id             int       `json:"id" gorm:"primaryKey"`
+	OrderNumber    string    `json:"order_number"`
+	OrderDate      time.Time `json:"order_date"`
+	EmployeeId     int       `json:"employee_id"`
+	ProductBatchId int       `json:"product_batch_id"`
+	WarehouseId    int       `json:"warehouse_id"`
 }
 
-func NewInboundOrder(id int, orderNumber string, employeeId int, productBathId int, warehouseId int) *InboundOrder {
+func NewInboundOrder(id int, orderNumber string, orderDate time.Time, employeeId int, productBathId int, warehouseId int) *InboundOrder {
 	return &InboundOrder{
 		Id:             id,
 		OrderNumber:    orderNumber,
+		OrderDate:      orderDate,
 		EmployeeId:     employeeId,
 		ProductBatchId: productBathId,
 		WarehouseId:    warehouseId,
