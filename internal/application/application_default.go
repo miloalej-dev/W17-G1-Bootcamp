7c47bc53524cc1862009111f670f30e78d3c6699
jalvarezbelt_meli <juan.alvarezbeltran@mercadolibre.com.co>
2025-07-27 20:46:58 -0500

    Implementation: Added Unit test for productBatch, modified names of the newproductBatchRepository to match project structure and refactored some methods to test them easily since we are not going to consume them at any point.

--- internal/application/application_default.go
+++ internal/application/application_default.go
@@ -54,7 +54,7 @@ func (a *ServerChi) Run() (err error) {
 	productRepository := database.NewProductDB(db)
 	warehouseRepository := database.NewWarehouseDB(db)
 	carrierRepository := database.NewCarrierDB(db)
-	productBatchRepository := database.NewProductBatchDB(db)
+	productBatchRepository := database.NewProductBatchRepository(db)
 	sellerRepository := database.NewSellerRepository(db)
 	employeeRepository := database.NewEmployeeRepository(db)
 	buyerRepository := database.NewBuyerRepository(db)
@@ -67,7 +67,7 @@ func (a *ServerChi) Run() (err error) {
 
 	productRecordService := _default.NewProductRecordDefault(productRecordRepository)
 	productService := _default.NewProductDefault(productRepository)
-  	warehouseService := _default.NewWarehouseDefault(warehouseRepository)
+	warehouseService := _default.NewWarehouseDefault(warehouseRepository)
 	carrierService := _default.NewCarrierDefault(carrierRepository)
 	productBatchService := _default.NewProductBatchDefault(productBatchRepository)
 	buyerService := _default.NewBuyerDefault(buyerRepository)
