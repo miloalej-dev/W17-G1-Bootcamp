ac9f603698969b5e59d8a810b66bfed74e2cafa6
bocastellano_meli <boris.castellanos@mercadolibre.com.co>
2025-07-28 09:34:19 -0500

    test: purchase order  handler test

--- internal/handler/purchase_order_test.go
+++ internal/handler/purchase_order_test.go
@@ -1,17 +1,23 @@
 package handler
 
 import (
+	"encoding/json"
+	"fmt"
 	"github.com/miloalej-dev/W17-G1-Bootcamp/pkg/models"
+	"github.com/miloalej-dev/W17-G1-Bootcamp/pkg/response"
 	"github.com/stretchr/testify/mock"
 	"github.com/stretchr/testify/suite"
+	"net/http"
+	"net/http/httptest"
 	"testing"
+	"time"
 )
 
 type PurchaseOrderServiceMock struct {
 	mock.Mock
 }
 
-type PurchaseOrderTestSuite struct {
+type PurchaseOrderHandlerTestSuite struct {
 	suite.Suite
 	mock    *PurchaseOrderServiceMock
 	handler *PurchaseOrderHandler
@@ -53,7 +59,7 @@ func (s *PurchaseOrderServiceMock) RetrieveByBuyer(id int) ([]models.PurchaseOrd
 	return args.Get(0).([]models.PurchaseOrder), args.Error(1)
 }
 
-func (s *PurchaseOrderTestSuite) SetupTest() {
+func (s *PurchaseOrderHandlerTestSuite) SetupTest() {
 	s.mock = new(PurchaseOrderServiceMock)
 	s.handler = NewPurchaseOrderDefault(s.mock)
 	s.path = "/api/v1/purchase_orders"
@@ -61,5 +67,48 @@ func (s *PurchaseOrderTestSuite) SetupTest() {
 
 // Run the test suite
 func TestSellerHandlerTestSuite(t *testing.T) {
-	suite.Run(t, new(PurchaseOrderTestSuite))
+	suite.Run(t, new(PurchaseOrderHandlerTestSuite))
+}
+
+func (s *PurchaseOrderHandlerTestSuite) TestGetPurchaseOrdersReport_Ok() {
+	// Arrange
+	id := 1
+	expectedData := []models.PurchaseOrder{
+		{
+			Id:            1,
+			OrderNumber:   "PO123",
+			OrderDate:     time.Now(),
+			TracingCode:   "ABC123",
+			BuyerID:       id,
+			WarehouseID:   1,
+			CarrierID:     2,
+			OrderStatusID: 1,
+			OrderDetails: &[]models.OrderDetail{
+				{
+					Id:               1,
+					Quantity:         10,
+					CleanLinesStatus: "clean",
+					Temperature:      4.5,
+					ProductRecordID:  1001,
+					PurchaseOrderID:  1,
+				},
+			},
+		},
+	}
+
+	expectedResponse := response.Response{Data: expectedData}
+	expectedBody, _ := json.Marshal(expectedResponse)
+
+	// Mock del servicio
+	s.mock.On("RetrieveByBuyer", id).Return(expectedData, nil)
+
+	req := httptest.NewRequest(http.MethodGet, fmt.Sprintf("%s/report?id=%d", s.path, id), nil)
+	rec := httptest.NewRecorder()
+
+	// Act
+	s.handler.GetPurchaseOrdersReport(rec, req)
+
+	// Assert
+	s.Equal(http.StatusOK, rec.Code)
+	s.JSONEq(string(expectedBody), rec.Body.String())
 }
