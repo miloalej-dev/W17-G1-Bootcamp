88262289c8eaba4c22002e409c40901090a6522b
jparraareval_meli <juan.parraarevalo@mercadolibre.com.co>
2025-07-28 17:52:52 -0500

    refactor: refactor tests

--- pkg/request/warehouse_test.go
+++ pkg/request/warehouse_test.go
@@ -7,100 +7,117 @@ import (
 )
 
 func Test_Bind(t *testing.T) {
-	t.Run("success", func(t *testing.T){
 
-		code := "WAR-001"
-		address := "Calle estación"
-		telephone := "111-222333"
-		minimumCapacity := 10
-		minimumTemperature := 1
-		locality_id := 1
+	code := "WAR-001"
+	address := "Calle estación"
+	telephone := "111-222333"
+	minimumCapacity := 10
+	minimumTemperature := 1
+	locality_id := 1
 
-		warehouse := WarehouseRequest {
-			WarehouseCode:      &code,
-			Address:            &address,
-			Telephone:          &telephone,
-			MinimumCapacity:    &minimumCapacity,
-			MinimumTemperature: &minimumTemperature,
-			LocalityId:         &locality_id,
-		}
+	tests := []struct {
+		title         string
+		request       *WarehouseRequest
+		expectedError string
+	}{
+		{
+			title: "Success - All fields valid",
+			request: &WarehouseRequest {
+				WarehouseCode:      &code,
+				Address:            &address,
+				Telephone:          &telephone,
+				MinimumCapacity:    &minimumCapacity,
+				MinimumTemperature: &minimumTemperature,
+				LocalityId:         &locality_id,
+			},
+			expectedError: "",
+		},
+		{
+			title: "Error - Missing code",
+			request: &WarehouseRequest {
+				WarehouseCode:      nil,
+				Address:            &address,
+				Telephone:          &telephone,
+				MinimumCapacity:    &minimumCapacity,
+				MinimumTemperature: &minimumTemperature,
+				LocalityId:         &locality_id,
+			},
+			expectedError: "warehouse code must not be null",
+		},
+		{
+			title: "Error - Missing address",
+			request: &WarehouseRequest {
+				WarehouseCode:      &code,
+				Address:            nil,
+				Telephone:          &telephone,
+				MinimumCapacity:    &minimumCapacity,
+				MinimumTemperature: &minimumTemperature,
+				LocalityId:         &locality_id,
+			},
+			expectedError: "address must not be null",
+		},
+		{
+			title: "Error - Missing telephone",
+			request: &WarehouseRequest {
+				WarehouseCode:      &code,
+				Address:            &address,
+				Telephone:          nil,
+				MinimumCapacity:    &minimumCapacity,
+				MinimumTemperature: &minimumTemperature,
+				LocalityId:         &locality_id,
+			},
+			expectedError: "telephone must not be null",
+		},
+		{
+			title: "Error - Missing minimum capacity",
+			request: &WarehouseRequest {
+				WarehouseCode:      &code,
+				Address:            &address,
+				Telephone:          &telephone,
+				MinimumCapacity:    nil,
+				MinimumTemperature: &minimumTemperature,
+				LocalityId:         &locality_id,
+			},
+			expectedError: "minimum Capacity must not be null",
+		},
+		{
+			title: "Error - Missing minimum temperature",
+			request: &WarehouseRequest {
+				WarehouseCode:      &code,
+				Address:            &address,
+				Telephone:          &telephone,
+				MinimumCapacity:    &minimumCapacity,
+				MinimumTemperature: nil,
+				LocalityId:         &locality_id,
+			},
+			expectedError: "minimum Temperature must not be null",
+		},
+		{
+			title: "Error - Missing locality id",
+			request: &WarehouseRequest {
+				WarehouseCode:      &code,
+				Address:            &address,
+				Telephone:          &telephone,
+				MinimumCapacity:    &minimumCapacity,
+				MinimumTemperature: &minimumTemperature,
+				LocalityId:         nil,
+			},
+			expectedError: "locality id must not be null",
+		},
+	}
 
-		err := warehouse.Bind(&http.Request{})
-		require.NoError(t, err)
-	})
+	for _, tc := range tests {
+		t.Run(tc.title, func(t *testing.T) {
+			// Act
+			err := tc.request.Bind(&http.Request{})
 
-	t.Run("nil fields", func(t *testing.T) {
-
-		code := ""
-		address := ""
-		telephone := ""
-		var minimumCapacity int
-		var minimumTemperature int
-
-		warehouse := WarehouseRequest {
-			WarehouseCode:      nil,
-			Address:            nil,
-			Telephone:          nil,
-			MinimumCapacity:    nil,
-			MinimumTemperature: nil,
-			LocalityId:         nil,
-		}
-		err := warehouse.Bind(&http.Request{})
-		require.Error(t, err)
-
-		warehouse = WarehouseRequest {
-			WarehouseCode:      &code,
-			Address:            nil,
-			Telephone:          nil,
-			MinimumCapacity:    nil,
-			MinimumTemperature: nil,
-			LocalityId:         nil,
-		}
-		err = warehouse.Bind(&http.Request{})
-		require.Error(t, err)
-
-		warehouse = WarehouseRequest {
-			WarehouseCode:      &code,
-			Address:            &address,
-			Telephone:          nil,
-			MinimumCapacity:    nil,
-			MinimumTemperature: nil,
-			LocalityId:         nil,
-		}
-		err = warehouse.Bind(&http.Request{})
-		require.Error(t, err)
-
-		warehouse = WarehouseRequest {
-			WarehouseCode:      &code,
-			Address:            &address,
-			Telephone:          &telephone,
-			MinimumCapacity:    nil,
-			MinimumTemperature: nil,
-			LocalityId:         nil,
-		}
-		err = warehouse.Bind(&http.Request{})
-		require.Error(t, err)
-
-		warehouse = WarehouseRequest {
-			WarehouseCode:      &code,
-			Address:            &address,
-			Telephone:          &telephone,
-			MinimumCapacity:    &minimumCapacity,
-			MinimumTemperature: nil,
-			LocalityId:         nil,
-		}
-		err = warehouse.Bind(&http.Request{})
-		require.Error(t, err)
-
-		warehouse = WarehouseRequest {
-			WarehouseCode:      &code,
-			Address:            &address,
-			Telephone:          &telephone,
-			MinimumCapacity:    &minimumCapacity,
-			MinimumTemperature: &minimumTemperature,
-			LocalityId:         nil,
-		}
-		err = warehouse.Bind(&http.Request{})
-		require.Error(t, err)
-	})
+			// Assert
+			if tc.expectedError == "" {
+				require.NoError(t, err)
+			} else {
+				require.Error(t, err)
+				require.Equal(t, tc.expectedError, err.Error())
+			}
+		})
+	}
 }
