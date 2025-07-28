477844195b27f2d104c2f1c7c0790aee74c47471
jparraareval_meli <juan.parraarevalo@mercadolibre.com.co>
2025-07-28 17:45:54 -0500

    refactor: refactor tests

--- pkg/request/carrier_test.go
+++ pkg/request/carrier_test.go
@@ -7,91 +7,98 @@ import (
 )
 
 func Test_Bind(t *testing.T) {
-	t.Run("success", func(t *testing.T){
 
-		cId := "CID-001"
-		companyName := "Meli"
-		address	:= "Dirección"
-		telephone := "111-222333"
-		locality_id := 1
+	cId := "CID-001"
+	companyName := "Meli"
+	address	:= "Dirección"
+	telephone := "111-222333"
+	locality_id := 1
 
-		carrier := CarrierRequest {
-			CId:      			&cId,
-			CompanyName:        &companyName,
-			Address:            &address,
-			Telephone:          &telephone,
-			LocalityId:         &locality_id,
-		}
+	tests := []struct {
+		title         string
+		request       *CarrierRequest
+		expectedError string
+	}{
+		{
+			title: "Success - All fields valid",
+			request: &CarrierRequest {
+				CId:      			&cId,
+				CompanyName:        &companyName,
+				Address:            &address,
+				Telephone:          &telephone,
+				LocalityId:         &locality_id,
+			},
+			expectedError: "",
+		},
+		{
+			title: "Error - missing CId",
+			request: &CarrierRequest {
+				CId:      			nil,
+				CompanyName:        &companyName,
+				Address:            &address,
+				Telephone:          &telephone,
+				LocalityId:         &locality_id,
+			},
+			expectedError: "cid code must not be null",
+		},
+		{
+			title: "Error - missing company name",
+			request: &CarrierRequest {
+				CId:      			&cId,
+				CompanyName:        nil,
+				Address:            &address,
+				Telephone:          &telephone,
+				LocalityId:         &locality_id,
+			},
+			expectedError: "company name must not be null",
+		},
+		{
+			title: "Error - missing address",
+			request: &CarrierRequest {
+				CId:      			&cId,
+				CompanyName:        &companyName,
+				Address:            nil,
+				Telephone:          &telephone,
+				LocalityId:         &locality_id,
+			},
+			expectedError: "address must not be null",
+		},
+		{
+			title: "Error - missing telephone",
+			request: &CarrierRequest {
+				CId:      			&cId,
+				CompanyName:        &companyName,
+				Address:            &address,
+				Telephone:          nil,
+				LocalityId:         &locality_id,
+			},
+			expectedError: "telephone must not be null",
+		},
+		{
+			title: "Error - missing locality Id",
+			request: &CarrierRequest {
+				CId:      			&cId,
+				CompanyName:        &companyName,
+				Address:            &address,
+				Telephone:          &telephone,
+				LocalityId:         nil,
+			},
+			expectedError: "locality id must not be null",
+		},
+	}
 
-		err := carrier.Bind(&http.Request{})
-		require.NoError(t, err)
-	})
+	for _, tc := range tests {
+		t.Run(tc.title, func(t *testing.T) {
+			// Act
+			err := tc.request.Bind(&http.Request{})
 
-	t.Run("nil fields", func(t *testing.T) {
-
-		cId := "CID-001"
-		companyName := "Meli"
-		address	:= "Dirección"
-		telephone := "111-222333"
-
-		carrier := CarrierRequest {
-			CId:      			nil,
-			CompanyName:        nil,
-			Address:            nil,
-			Telephone:          nil,
-			LocalityId:         nil,
-		}
-		err := carrier.Bind(&http.Request{})
-		require.Error(t, err)
-
-		carrier = CarrierRequest {
-			CId:      			&cId,
-			CompanyName:        nil,
-			Address:            nil,
-			Telephone:          nil,
-			LocalityId:         nil,
-		}
-		err = carrier.Bind(&http.Request{})
-		require.Error(t, err)
-
-		carrier = CarrierRequest {
-			CId:      			&cId,
-			CompanyName:        &companyName,
-			Address:            nil,
-			Telephone:          nil,
-			LocalityId:         nil,
-		}
-		err = carrier.Bind(&http.Request{})
-		require.Error(t, err)
-
-		carrier = CarrierRequest {
-			CId:      			&cId,
-			CompanyName:        &companyName,
-			Address:            &address,
-			Telephone:          nil,
-			LocalityId:         nil,
-		}
-		err = carrier.Bind(&http.Request{})
-		require.Error(t, err)
-
-		carrier = CarrierRequest {
-			CId:      			&cId,
-			CompanyName:        &companyName,
-			Address:            &address,
-			Telephone:          &telephone,
-			LocalityId:         nil,
-		}
-		err = carrier.Bind(&http.Request{})
-		require.Error(t, err)
-
-		carrier = CarrierRequest {
-			CId:      			&cId,
-			CompanyName:        &companyName,
-			Address:            &address,
-			Telephone:          &telephone,
-			LocalityId:         nil,
-		}
-		err = carrier.Bind(&http.Request{})
-		require.Error(t, err)
-	})
+			// Assert
+			if tc.expectedError == "" {
+				require.NoError(t, err)
+			} else {
+				require.Error(t, err)
+				require.Equal(t, tc.expectedError, err.Error())
+			}
+		})
+	}
 }
